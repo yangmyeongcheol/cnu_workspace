4ef9a7d01e8409aabaea9dc9da91f83936328ca0
myeongcheolyang <skduddlsn123@naver.com>
2022-05-31 10:02:27 +0900

    test1

--- library.py
+++ library.py
@@ -7,6 +7,8 @@
 
 import sys
 import service.BookService as bookService
+import service.MemberService as memberService
+
 while True:
     print('::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
     print(':: CNU LIBRARY')
@@ -41,7 +43,7 @@ while True:
     elif select_num == 4:  # 도서 반납
         pass
     elif select_num == 5:  # 회원 목록
-        pass
+        memberService.get_members()
     elif select_num == 6:  # 회원 검색
         pass
     elif select_num == 7:  # 프로그램 종료
---
+++ service/MemberService.py
@@ -0,0 +1,21 @@
+from config.config_db import connection_db
+
+
+ # 회원목록 조회
+def get_members():
+    conn = connection_db()
+
+    try:
+        curs = conn.cursor()
+        sql = '''SELECT * FROM tbl_member'''
+        curs.execute(sql)
+        rows = curs.fetchall()
+    finally:
+        conn.close()
+
+    print(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
+    print(':: ID\tNAME\tPHONE\tDATE')
+    print(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
+    for row in rows:
+        print(f'::{row.values()}')
+    print(':::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::')
